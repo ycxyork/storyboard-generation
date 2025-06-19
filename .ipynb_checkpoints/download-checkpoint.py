66034124c3430fb2f95a98d55ce6210a27361f43
ycxyork <1443682107@qq.com>
2025-06-19 08:02:04 +0000

    Track large model weights with git lfs

--- .ipynb_checkpoints/download-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="alimama-creative/FLUX.1-dev-Controlnet-Inpainting-Beta",
-    local_dir="model/alimama-creative",
-    local_dir_use_symlinks=False
-)
